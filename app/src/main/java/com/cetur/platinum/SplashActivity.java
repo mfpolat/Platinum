ef538c1655234a260770bd713c725f18140188e0
fatih polat <m.fatih.polat.27@gmail.com>
2015-07-01 02:25:41 +0300

    ilk commit

---
+++ app/src/main/java/com/cetur/platinum/SplashActivity.java
@@ -0,0 +1,62 @@
+package com.cetur.platinum;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    private final long startTime = 3000;
+    private final long interval = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        SplashTimer timer = new SplashTimer(startTime,interval);
+        timer.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private class SplashTimer extends CountDownTimer{
+
+        public SplashTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long l) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            finish();
+            startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+        }
+    }
+}
